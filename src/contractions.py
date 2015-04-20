e213ec82010c93c78dbe7cce1590bd0d288b5035
Jordan Williams <jwill49@uic.edu>
2015-04-20 14:09:23 -0500

    Added contraction rules

---
+++ src/contractions.py
@@ -0,0 +1,98 @@
+# has/have or is
+BE = [
+	["I'm", "I am"],
+	["you're", "you are"],
+	["he's", "he is"],
+	["she's", "she is"],
+	["it's", "it is"],
+	["we're", "we are"],
+	["they're", "they are"],
+	["that's", "that is"],
+	["who's", "who is"],
+	["what're", "what are"],
+	["what's", "what is"],
+	["where's", "where is"],
+	["when's", "when is"],
+	["why's", "why is"],
+	["how's", "how is"]
+]
+
+WILL = [
+	["I'll", "I will"],
+	["you'll", "you will"],
+	["he'll", "he will"],
+	["she'll", "she will"],
+	["it'll", "it will"],
+	["we'll", "we will"],
+	["they'll", "they will"],
+	["that'll", "that will"],
+	["who'll", "who will"],
+	["what'll", "what will"],
+	["where'll", "where will"],
+	["when'll", "when will"],
+	["why'll", "why will"],
+	["how'll", "how will"]
+]
+
+# would or had
+WOULD = [
+	["I'd", "I would"],
+	["you'd", "you would"],
+	["he'd", "he would"],
+	["she'd", "she would"],
+	["it'd", "it would"],
+	["we'd", "we would"],
+	["they'd", "they would"],
+	["that'd", "that would"],
+	["who'd", "who would"],
+	["what'd", "what would"],
+	["where'd", "where would"],
+	["when'd", "when would"],
+	["why'd", "why would"],
+	["how'd", "how would"]
+]
+
+# has/have or is
+HAVE = [
+	["I've", "I have"],
+	["you've", "you have"],
+	["he's", "he has"],
+	["she's", "she is"],
+	["it's", "it has"],
+	["we've", "we have"],
+	["they've", "they have"],
+	["that's", "that"],
+	["who's", "who is"],
+	["what's", "what is"],
+	["where's", "where is"],
+	["when's", "when has"],
+	["why's", "why has"],
+	["how's", "how has"],
+	["would've", "would have"],
+	["should've", "should have"],
+	["could've", "could have"],
+	["might've", "might have"],
+	["must've", "must have"]
+]
+
+# had or would
+HAD = [
+	["I'd", "I had"],
+	["you'd", "you had"],
+	["he'd", "he had"],
+	["she'd", "she had"],
+	["it'd", "it had"],
+	["we'd", "we had"],
+	["they'd", "they had"],
+	["that'd", "that had"],
+	["who'd", "who had"],
+	["what'd", "what had"],
+	["where'd", "where had"],
+	["when'd", "when had"],
+	["why'd", "why had"],
+	["how'd", "how had"]
+]
+
+NOT = [
+	["n't", "not"]
+]
